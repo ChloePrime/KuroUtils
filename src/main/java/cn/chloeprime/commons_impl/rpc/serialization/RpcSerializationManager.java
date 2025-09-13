cc644c188587426430a99b38e09c5d562c016dcf
Chloe Prime <chloe_von_einzbern@qq.com>
2025-09-14 01:34:01 +0800

    修复基础类型为接口的序列化器无法被序列化的bug

--- src/main/java/cn/chloeprime/commons_impl/rpc/serialization/RpcSerializationManager.java
+++ src/main/java/cn/chloeprime/commons_impl/rpc/serialization/RpcSerializationManager.java
@@ -4,8 +4,8 @@ import cn.chloeprime.commons.rpc.exception.ParameterSerializationException;
 import cn.chloeprime.commons_impl.rpc.LambdaReflectResult;
 import net.minecraft.network.FriendlyByteBuf;
 
-import java.util.Map;
-import java.util.Objects;
+import java.util.*;
+import java.util.concurrent.ConcurrentHashMap;
 
 public class RpcSerializationManager {
     @SuppressWarnings("unchecked")
@@ -38,19 +38,56 @@ public class RpcSerializationManager {
             Double.class, double.class
     );
 
+    private static final ThreadLocal<Deque<Set<RpcParameterSerializer<?>>>> INTERFACE_RESULTS_OBJECT_POOL = ThreadLocal.withInitial(ArrayDeque::new);
+    private static final Map<Class<?>, RpcParameterSerializer<?>> CLASS_TO_SERIALIZER = new ConcurrentHashMap<>();
+
     @SuppressWarnings("rawtypes")
     private static <T> RpcParameterSerializer getSerializerFor(Class<? extends T> argType) {
-        var result = (Class<?>) Objects.requireNonNull(argType);
-        while (result != null) {
-            var serializer = RpcSerializers.BY_TYPE.get(result);
-            if (serializer != null) {
-                return serializer;
+        return CLASS_TO_SERIALIZER.computeIfAbsent(argType, RpcSerializationManager::getSerializerFor0);
+    }
+
+    @SuppressWarnings("rawtypes")
+    private static <T> RpcParameterSerializer getSerializerFor0(Class<? extends T> argType) {
+        var clazz = (Class<?>) Objects.requireNonNull(argType);
+        var setPool = INTERFACE_RESULTS_OBJECT_POOL.get();
+        Set<RpcParameterSerializer<?>> interfaceResults = Objects.requireNonNullElseGet(setPool.poll(), LinkedHashSet::new);
+        try {
+            while (clazz != null) {
+                var serializer = RpcSerializers.BY_TYPE.get(clazz);
+                if (serializer != null) {
+                    return serializer;
+                }
+                // 处理可被序列化的接口
+                // 如果本类直接实现的接口发现序列化器，那么remove上一次计算（它的子类）的结果
+                var thisClassHasInterfaceResult = false;
+                for (Class<?> i : clazz.getInterfaces()){
+                    var interfaceSerializer = RpcSerializers.BY_TYPE.get(i);
+                    if (interfaceSerializer != null) {
+                        if (!thisClassHasInterfaceResult) {
+                            thisClassHasInterfaceResult = true;
+                            interfaceResults.clear();
+                        }
+                        interfaceResults.add(interfaceSerializer);
+                    }
+                }
+                clazz = clazz.getSuperclass();
             }
-            result = result.getSuperclass();
+            if (argType.isArray()) {
+                return getSerializerFor(argType.componentType()).arrayType();
+            }
+            if (interfaceResults.size() == 1) {
+                return interfaceResults.iterator().next();
+            }
+            if (interfaceResults.size() > 1) {
+                throw new ParameterSerializationException("Unsupported parameter type: %s, it has more than one (%d) serializable interfaces".formatted(
+                        argType.getCanonicalName(), interfaceResults.size()
+                ));
+            } else {
+                throw new ParameterSerializationException("Unsupported parameter type: " + argType.getCanonicalName());
+            }
+        } finally {
+            interfaceResults.clear();
+            setPool.push(interfaceResults);
         }
-        if (argType.isArray()) {
-            return getSerializerFor(argType.componentType()).arrayType();
-        }
-        throw new ParameterSerializationException("Unsupported parameter type: " + argType.getCanonicalName());
     }
 }
