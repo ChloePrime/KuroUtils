564de0a9a005d6837233e441029bb2e8080627c6
Chloe Prime <chloe_von_einzbern@qq.com>
2025-09-14 15:32:05 +0800

    - 让 RPCTarget 相关异常全部使用 UnsupportedRpcOperationException 而非 UnsupportedOperationException - 让 RPCTarget#reply() 在客户端非 RPC 方法调用时也报错

--- src/main/java/cn/chloeprime/commons/rpc/RPCContext.java
+++ src/main/java/cn/chloeprime/commons/rpc/RPCContext.java
@@ -1,14 +1,16 @@
 package cn.chloeprime.commons.rpc;
 
 import cn.chloeprime.commons.rpc.exception.RpcException;
+import cn.chloeprime.commons.rpc.exception.UnsupportedRpcOperationException;
 import cn.chloeprime.commons_impl.rpc.Endpoint;
 import cn.chloeprime.commons_impl.rpc.RpcSupport;
 import cn.chloeprime.commons_impl.rpc.packet.RpcCallMethodPacket;
 import cn.chloeprime.commons_impl.CommonProxy;
 import net.minecraft.server.level.ServerPlayer;
+import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
-import java.util.Objects;
+import java.util.Optional;
 
 /**
  * Context of an RPC call.
@@ -18,18 +20,21 @@ public class RPCContext {
      * Get the caller of the RPC call.
      *
      * @return the caller of the RPC call.
+     * @throws UnsupportedRpcOperationException when called from non-RPC methods.
      */
-    public static Endpoint getSender() {
-        return Objects.requireNonNull(RpcCallMethodPacket.CONTEXT.get().peek(), "RPCContext.getSender() can only be called in an RPC method!");
+    public static @NotNull Endpoint getSender() {
+        return Optional
+                .ofNullable(RpcCallMethodPacket.CONTEXT.get().peek())
+                .orElseThrow(() -> new UnsupportedRpcOperationException("RPCContext.getSender() can only be called in an RPC method!"));
     }
 
     /**
      * Get the caller of the RPC call as a server player.
      *
      * @return the caller of the RPC call, {@code null} if this call is called from the server.
+     * @throws UnsupportedRpcOperationException when called from non-RPC methods.
      */
-    @Nullable
-    public static ServerPlayer getSenderPlayer() {
+    public static @Nullable ServerPlayer getSenderPlayer() {
         var sender = getSender();
         if (sender.isServer()) {
             final var msg = "Cannot invoke getSenderPlayer at client side!";
@@ -38,5 +43,6 @@ public class RPCContext {
         return CommonProxy.getPlayerByUUID(sender.id()) instanceof ServerPlayer ssp ? ssp : null;
     }
 
-    private RPCContext() {}
+    private RPCContext() {
+    }
 }
