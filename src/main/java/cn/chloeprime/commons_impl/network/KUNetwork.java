968ac26e5c00e2092150c0cb85dd707b195fe67e
Chloe Prime <chloe_von_einzbern@qq.com>
2026-05-16 01:43:57 +0800

    [1.20.1] 更新网络协议版本至 2.5.0

--- src/main/java/cn/chloeprime/commons_impl/network/KUNetwork.java
+++ src/main/java/cn/chloeprime/commons_impl/network/KUNetwork.java
@@ -8,7 +8,7 @@ import net.minecraftforge.network.NetworkRegistry;
 import net.minecraftforge.network.simple.SimpleChannel;
 
 public class KUNetwork {
-    public static final String VERSION = "1.0.0";
+    public static final String VERSION = "2.5.0";
 
     public static final SimpleChannel CHANNEL = NetworkRegistry.newSimpleChannel(
             KuroUtilsMod.loc("play_channel"),
